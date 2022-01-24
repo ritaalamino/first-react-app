cbbdb3c114749c5db9e7222b5a192f0711fe08aa
Rita Alamino <ritaalamino@gmail.com>
2022-01-24 17:46:17 -0300

    responsivity

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -58,7 +58,7 @@ export function Home() {
                     <img src={logoImg} alt="Letmeask" />
                     <button onClick={handleCreateRoom} className="create-room">
                         <img src={googleIconImg} alt="Logo do Google" />
-                        Crie sua sala com o Google
+                        <span className="label">Crie sua sala com o Google </span>
                     </button>
                     <div className="separator">ou entre em uma sala</div>
                     <form onSubmit={handleJoinRoom}>
